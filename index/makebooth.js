a8cdc257e2b1b10407ee1f973ce33b7b6db09329
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2025-04-07 10:44:07 +0900

    부스 생성 모듈화
    booths.js에 createBooth라는 함수로
    부스 생성을 더 쉽게 변환

---
+++ index/makebooth.js
@@ -0,0 +1,43 @@
+class Booth {
+    constructor(id, imgSrc, imgWidth, imgHeight, title, description) {
+        this.id = id;
+        this.imgSrc = imgSrc;
+        this.imgWidth = imgWidth;
+        this.imgHeight = imgHeight;
+        this.title = title;
+        this.description = description;
+    }
+
+    createBoothElement() {
+        const container = document.createElement('div');
+        container.id = this.id;
+        container.style.display = 'none';
+
+        const innerDiv = document.createElement('div');
+
+        const img = document.createElement('img');
+        img.src = this.imgSrc;
+        img.width = this.imgWidth;
+        img.height = this.imgHeight;
+
+        const h2 = document.createElement('h2');
+        h2.textContent = this.title;
+
+        const p = document.createElement('p');
+        p.textContent = this.description;
+
+        innerDiv.appendChild(img);
+        innerDiv.appendChild(h2);
+        innerDiv.appendChild(p);
+        container.appendChild(innerDiv);
+
+        return container;
+    }
+}
+
+function createBooth(id, imgSrc, imgWidth, imgHeight, title, description) {
+    const booth = new Booth(id, imgSrc, imgWidth, imgHeight, title, description);
+    const boothElement = booth.createBoothElement();
+    document.body.appendChild(boothElement);
+    return boothElement;
+}
