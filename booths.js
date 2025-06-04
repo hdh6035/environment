c8e76f729d85dd1350631bd9660fed42d0f29b64
hdh6035 <ejrghks2002@naver.com>
2025-06-04 11:47:28 +0900

    Update booths.js

--- booths.js
+++ booths.js
@@ -1,11 +1,11 @@
 const booths = [
     {
         id: '1',
-        img: 'https://i.postimg.cc/Z5NqXNSf/image.png',
+        img: 'https://i.postimg.cc/0273vtjK/ESD.png',
         width: 300,
         height: 200,
-        title: '화분 컵케이크 만들기',
-        description: '화분 모양의 컵에 식용 재료로 케이크를 만들고, 토핑을 흙과 식물처럼 꾸며보는 창의적 베이킹 체험!<br>디저트를 만들며 환경의 소중함까지 함께 배워보는 시간입니다.'
+        title: '오호~ 친환경 물병이다!'<br>'(오호 만들기)',
+        description: '해조류 성분으로 만든 신기한 친환경 물병, 오호(Ooho)! 직접 만들고 지구를 지키는 한 모금의 가치를 만들어보세요.'
     },
     {
         id: '2',
@@ -46,6 +46,38 @@ const booths = [
         height: 200,
         title: '지구를 위한 퀴즈, 나를 위한 소원',
         description: 'QR코드를 스캔해 환경 퀴즈 5문제에 도전하세요!(소정의 선물 있어요!!)<br>퀴즈를 마친 참가자는 친환경 재생지 카드에 나의 환경 실천 다짐 또는 지구에게 전하고 싶은 한 마디를 적어봐요.'
+    },
+    {
+        id: '7',
+        img: 'https://i.postimg.cc/TYp1qSHv/image.png',
+        width: 300,
+        height: 200,
+        title: '방긋방긋 바다친구 만나기',
+        description: '거제지역에 서식하는 해양보호종인 상괭이(돌고래)보호를 위한 체험부스<br>상쾡이 엽서만들기, 상괭이 포토존 운영 등'
+    },
+    {
+        id: '8',
+        img: 'https://i.postimg.cc/TYp1qSHv/image.png',
+        width: 300,
+        height: 200,
+        title: '방긋방긋 바다친구 만나기',
+        description: '거제지역에 서식하는 해양보호종인 상괭이(돌고래)보호를 위한 체험부스<br>상쾡이 엽서만들기, 상괭이 포토존 운영 등'
+    },
+    {
+        id: '9',
+        img: 'https://i.postimg.cc/TYp1qSHv/image.png',
+        width: 300,
+        height: 200,
+        title: '방긋방긋 바다친구 만나기',
+        description: '거제지역에 서식하는 해양보호종인 상괭이(돌고래)보호를 위한 체험부스<br>상쾡이 엽서만들기, 상괭이 포토존 운영 등'
+    },
+    {
+        id: '10',
+        img: 'https://i.postimg.cc/TYp1qSHv/image.png',
+        width: 300,
+        height: 200,
+        title: '방긋방긋 바다친구 만나기',
+        description: '거제지역에 서식하는 해양보호종인 상괭이(돌고래)보호를 위한 체험부스<br>상쾡이 엽서만들기, 상괭이 포토존 운영 등'
     }
 ];
 
